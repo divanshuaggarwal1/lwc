dece606db33c3f3a9d9e0b21a62d2fa76d75c01a
divanshuaggarwal1 <107192165+divanshuaggarwal1@users.noreply.github.com>
2024-10-03 13:57:05 +0530

    Update pageThree.js

--- pageThree.js
+++ pageThree.js
@@ -1,13 +1,16 @@
 import { LightningElement, track } from 'lwc';
+import saveFieldData from '@salesforce/apex/DataTable.saveFieldData';
 export default class PageThree extends LightningElement {
     currentStep = '3';
     @track ind;
+     cardTitle = 'Data Virtualization - dd Object Configuration';
+
     @track data = [
         {
             id: 1,
             name: '',
-            type: '',
-            decimal: '',
+            fieldType: '',
+            decimalValue: '',
             length: '',
             filterable: false,
             externalId: false,
@@ -30,13 +33,6 @@ export default class PageThree extends LightningElement {
         { label: 'Phone', value: 'Phone' },
         { label: 'Percent', value: 'Percent' }
     ];
-    // decimalOptions = [
-    //     { label: '0', value: '0' },
-    //     { label: '1', value: '1' },
-    //     { label: '2', value: '2' },
-    //     { label: '3', value: '3' },
-
-    // ];
     decimalOptions= [];
   
         generateDecimalOptions(){  
@@ -44,39 +40,7 @@ export default class PageThree extends LightningElement {
                 this.decimalOptions.push({label: i.toString(), value: i.toString()})
             }
         }
-
     
-
-
-    // handleAddRow() {
-    //     const lastRow = this.data[this.data.length - 1];
-    
-    //     const isRowEmpty = !lastRow.name && !lastRow.type && !lastRow.decimal && !lastRow.length;
-    //     const isNameFilled = !!lastRow.name; 
-    
-    //     if (isRowEmpty || isNameFilled) {
-    //         const newId = this.data.length + 1;
-    //         this.data = [
-    //             ...this.data,
-    //             {
-    //                 id: newId,
-    //                 name: '',
-    //                 type: '',
-    //                 decimal: '',
-    //                 length: '',
-    //                 filterable: false,
-    //                 externalId: false,
-    //                 sortable: false,
-    //                 disableDelete: false
-    //             }
-    //         ];
-    //         this.setRowIndices();
-    //     } else {
-    //         console.log('Cannot add a new row. Please fill the "Name" field or clear the current row.');
-    //     }
-    // }
-    
-
     handleAddRow() {    
             const newId = this.data.length + 1;
             this.data = [
@@ -84,8 +48,8 @@ export default class PageThree extends LightningElement {
                 {
                     id: newId,
                     name: '',
-                    type: '',
-                    decimal: '',
+                    fieldType: '',
+                    decimalValue: '',
                     length: '',
                     filterable: false,
                     externalId: false,
@@ -100,74 +64,35 @@ export default class PageThree extends LightningElement {
        
     }
 
-    // handleInputChange(event) {
-    //     // console.log("test::"+ event);
-    //     const field = event.target.dataset.field;
-    //     //  console.log("testfield::", field);
-
-    //     const id = event.target.dataset.id;
-    //     //  console.log("testid::"+ id);
-
-    //     const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-    //     // console.log("testvalue::"+ value);
-
-
-    //     this.data = this.data.map((row) => {
-    //         if (row.id == id) {
-    //             return { ...row, [field]: value };
-    //         }
-    //         return row;
-    //     });
-
-    //     if (this.data[0].name && this.data[0].type && this.data[0].decimal && this.data[0].length) {
-    //         setTimeout(() => {
-    //             console.log('First row data:', JSON.stringify(this.data[0]));
-    //         }, 0);
-    //     }
-
-    // }
-
     handleInputChange(event) {
-        const field = event.target.dataset.field;
-        const id = event.target.dataset.id;
-        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+        const field = event.target.dataset.field;     
+          console.log("value::::" ,field);
 
+        const id = event.target.dataset.id;
+        console.log("value::::" ,id);
+        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+       console.log("value::::" ,JSON.stringify(value));
         this.data = this.data.map((row) => {
             if (row.id == id) {
                 const updatedRow = { ...row, [field]: value };
 
-                if (updatedRow.type === 'Text' || updatedRow.type === 'Date/Time' || updatedRow.type === 'Date' || updatedRow.type === 'Time' || updatedRow.type === 'TextArea'|| updatedRow.type === 'URL'|| updatedRow.type === 'Phone'|| updatedRow.type === 'Checkbox') {
+                if (updatedRow.fieldType === 'Text' || updatedRow.fieldType === 'Date/Time' || updatedRow.fieldType === 'Date' || updatedRow.fieldType === 'Time' || updatedRow.fieldType === 'TextArea'|| updatedRow.fieldType === 'URL'|| updatedRow.fieldType === 'Phone'|| updatedRow.fieldType === 'Checkbox') {
                     updatedRow.isDecimalDisabled = true;
                 } else {
                     updatedRow.isDecimalDisabled = false;
                 }
 
-                if (updatedRow.type === 'Date/Time' || updatedRow.type === 'Date' || updatedRow.type === 'Time' || updatedRow.type === 'TextArea' || updatedRow.type === 'URL'|| updatedRow.type === 'Phone'|| updatedRow.type === 'Checkbox') {
+                if (updatedRow.fieldType === 'Date/Time' || updatedRow.fieldType === 'Date' || updatedRow.fieldType === 'Time' || updatedRow.fieldType === 'TextArea' || updatedRow.fieldType === 'URL'|| updatedRow.fieldType === 'Phone'|| updatedRow.fieldType === 'Checkbox') {
                     updatedRow.isLengthDisabled = true;
                 } else {
                     updatedRow.isLengthDisabled = false;
                 }
 
-                // updatedRow.isDecimalDisabled = updatedRow.type === 'Text';
-                // updatedRow.isDecimalDisabled = updatedRow.type === 'Date/Time';
-                // updatedRow.isLengthDisabled = updatedRow.type === 'Date/Time';
-                // updatedRow.isDecimalDisabled = updatedRow.type === 'Date';
-                // updatedRow.isLengthDisabled = updatedRow.type === 'Date';
-                // updatedRow.isDecimalDisabled = updatedRow.type === 'Time';
-                // updatedRow.isLengthDisabled = updatedRow.type === 'Time';
-
-
-
-
                 return updatedRow;
             }
             return row;
         });
-        if (this.data[0].name && this.data[0].type && this.data[0].decimal && this.data[0].length) {
-                    setTimeout(() => {
-                        console.log('First row data:', JSON.stringify(this.data[0]));
-                    }, 0);
-                }
+               
     }
 
 
@@ -181,8 +106,8 @@ export default class PageThree extends LightningElement {
                     return {
                         ...row,
                         name: '',
-                        type: '',
-                        decimal: '',
+                        fieldType: '',
+                        decimalValue: '',
                         length: '',
                         filterable: false,
                         externalId: false,
@@ -204,20 +129,7 @@ export default class PageThree extends LightningElement {
         this.setRowIndices();
         this.generateDecimalOptions();
     }
-    // setRowIndices() {
-    //     this.data = this.data.map((row, index) => ({
-    //         ...row,
-    //         index: index + 1,
-    //         isDecimalDisabled: row.type === 'Text' ,
-    //         isDecimalDisabled: row.type === 'Date/Time' ,
-    //         isLengthDisabled: row.type ==='Date/Time',
-    //           isDecimalDisabled: row.type === 'Date' ,
-    //         isLengthDisabled: row.type ==='Date',
-    //           isDecimalDisabled: row.type === 'Time' ,
-    //         isLengthDisabled: row.type ==='Time'
 
-    //     }));
-    // }
     setRowIndices() {
         this.data = this.data.map((row, index) => ({
             ...row,
@@ -225,35 +137,29 @@ export default class PageThree extends LightningElement {
         }));
     }
 
-    handleNext() {
-        const lastRow = this.data[this.data.length - 1];
-        const isNameFilled = !!lastRow.name; 
-        if ( isNameFilled) {
-                    const newId = this.data.length + 1;
-                    this.data = [
-                        ...this.data,
-                        {
-                            id: newId,
-                            name: '',
-                            type: '',
-                            decimal: '',
-                            length: '',
-                            filterable: false,
-                            externalId: false,
-                            sortable: false,
-                            disableDelete: false,
-                            isDecimalDisabled: false,
-                            isLengthDisabled: false
-
-                        }
-                    ];
-                    this.setRowIndices();
-                    console.log('Activated');
-
-                } else {
-                    console.log('Cannot Activate. Please fill the "Name" field or delete the current row.');
+        handleNext() {
+            console.log('this.data = ',this.data);
+            let allRowsData = [];
+            let isAllRowsValid = true;
+            this.data.forEach(row => {
+                if (!row.name && !row.fieldType) {
+                   isAllRowsValid = false; 
                 }
-    }
+            });
+            if (isAllRowsValid && this.data.length > 0) {
+                console.log('All filled rows data:', JSON.stringify(this.data));
+                    saveFieldData({ fieldData: JSON.stringify(this.data) })
+                    .then((result) => {
+                        console.log('Apex result:', result);
+                        this.currentStep = 'next';
+                    })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                    });
+            } else {
+                console.log('Cannot Activate. Please fill all required fields for each row or delete invalid rows.');
+            }
+        }
     handlePrevious() {
         this.currentStep = '2';
     }
